7c3e061daee38279c0fb1b19426f6d531317111f
tejas588 <saitejasrepala@gmail.com>
2025-12-04 11:37:32 +0530

    name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import SplitPage from "./components/SplitPage";
 
